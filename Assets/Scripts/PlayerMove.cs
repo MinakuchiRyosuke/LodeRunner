9cd25f8f39dc2926850728ea5b5ed550dbff1b67
5F\ktc <>
2025-12-05 12:41:54 +0900

    一マス移動の実装

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float walkspeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float gridSize;
     Rigidbody2D rb;
     Vector2 inputVec;
     Vector3 movVec;
+    private bool isMoving = false;
 
     private void OnMove(InputValue value)
     {
+        if (isMoving) return;
         inputVec = value.Get<Vector2>();
+        Vector2 targetPos = transform.position;
+        targetPos += inputVec;
+        StartCoroutine(Move(targetPos));
     }
     private void Awake()
     {
@@ -18,10 +25,19 @@ public class PlayerMove : MonoBehaviour
     }
     void Update()
     {
-        movVec = new Vector3(inputVec.x * walkspeed, 0, 0);
+        
     }
-    private void FixedUpdate()
+    IEnumerator Move(Vector3 targetPos)
     {
-        rb.linearVelocity = movVec;
+        isMoving = true;
+
+        while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, walkSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPos;
+        isMoving = false;
     }
 }
